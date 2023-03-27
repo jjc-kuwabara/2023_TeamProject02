72f7d69d9f9272c43863b423df1a0f0f4f65695c
HAL <haru.yoshino818@gmail.com>
2023-03-27 14:06:28 +0900

    スクリプトの修正

--- Assets/Scripts/DeadAreaScript.cs
+++ Assets/Scripts/DeadAreaScript.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class DeadAreaScript : MonoBehaviour
 {
+
+    public GameObject checkpoint; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,8 +23,9 @@ public class DeadAreaScript : MonoBehaviour
         Debug.Log("侵入したオブジェクト名＝" + other.gameObject.name);
         if (other.gameObject.tag == "Player")
         {
+            
             Debug.Log("Playerでした");
-            other.transform.position = new Vector3(0, 2, 0); //リスの高さ
+            other.transform.position = new Vector3(0,2,0);
             other.transform.rotation = Quaternion.identity; //リスの回転
         }
 
