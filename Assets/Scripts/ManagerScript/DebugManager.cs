0f757b695d7a69aa4b5a559ca892488c2a612130
HAL <haru.yoshino818@gmail.com>
2023-03-28 11:12:16 +0900

    DebugManagerの改良

--- Assets/Scripts/ManagerScript/DebugManager.cs
+++ Assets/Scripts/ManagerScript/DebugManager.cs
@@ -20,4 +20,21 @@ public class DebugManager : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+    public void Clear()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            GameManager.Instance.GameClear();
+        }
+    }
+ 
+    public void DaM()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            GameManager.Instance.Damage(10);
+            GameManager.Instance.state_damage = false;
+        }
+    }
+
 }
