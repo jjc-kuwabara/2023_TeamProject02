93495d09710b0ccff6b5258fadb6db58ca4f6010
HAL <haru.yoshino818@gmail.com>
2023-03-30 10:10:01 +0900

    Enemyのスクリプトを追加

---
+++ Assets/Scripts/GameScript/EnemyWood.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWood : MonoBehaviour
+{
+
+    public int hitCount;
+    public GameObject[] kafun;
+
+    [SerializeField] bool hit;
+    [SerializeField] bool dead;
+
+    void Start()
+    {  
+    }
+
+    void Update()
+    {
+    }
+
+    public void Down()
+    {
+    }
+}
