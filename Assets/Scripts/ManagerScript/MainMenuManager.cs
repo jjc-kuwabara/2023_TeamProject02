5703f584027ce7f7bdca780f6d39d9665a0caf75
vally7469 <128009138+vally7469@users.noreply.github.com>
2023-04-17 14:02:45 +0900

    メインメニューの項目の追加

---
+++ Assets/Scripts/ManagerScript/MainMenuManager.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [Header("必要なキャンバス")]
+    [SerializeField] GameObject[] canvas;
+
+    [Header("各メニューの初期カーソル対象")]
+    [SerializeField] GameObject[] focusobject;
+
+    GameObject currentFocus; //現在選択している対象
+    GameObject previousFocus; //前フレームに選択していた対象
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CanvasInIt();
+
+        canvas[0].SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(focusobject[0]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FocusCheck();
+    }
+
+    void CanvasInIt()
+    {
+        //canvasGroupの要素数をCanvasの数と一緒にする。　canvasGroup = new CanvasGroup[canvas.length];
+
+        for(int i = 0; i < canvas.Length; i++)
+        {
+            canvas[i].SetActive(false);//すべてのメニューを非表示
+        }
+    }
+
+    //メニューの移動（各メニュー項目のイベントトリガーでキャンバスの番号を指定）
+    public void Transition_menu(int nextMenu)
+    {
+        CanvasInIt();
+
+        canvas[nextMenu].SetActive(true);//次のメニューを表示
+
+        EventSystem.current.SetSelectedGameObject(focusobject[nextMenu]);//次のメニューの初期カーソル位置
+         
+
+         
+    }
+    //フォーカス対象のチェック
+    void FocusCheck()
+    {
+        currentFocus = EventSystem.current.currentSelectedGameObject;//現在のフォーカスの項目の格納
+
+        if (currentFocus == previousFocus) return;//前回と変わらない場合は何もせず終了
+
+        if(currentFocus == null)//フォーカス対象がなかった場合、前フレームまで選択していた項目を選択状態に
+        {
+            EventSystem.current.SetSelectedGameObject(previousFocus);
+            return;
+        }
+
+        //残された条件からフォーカスしている項目が存在するのが確定
+        //前フレームの対象を更新する
+        previousFocus = EventSystem.current.currentSelectedGameObject;
+    }
+
+    //ゲーム終了の処理
+    public void Quit()
+    {
+        //unity上
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+//ビルドした実行データでプレイを終了する場合
+          Application.Quit();
+#endif
+
+
+
+    }
+}
