798a715c5d1f589a70ffa3ffd32eca1be3968ba1
vally7469 <128009138+vally7469@users.noreply.github.com>
2023-04-26 12:02:17 +0900

    ポーズ機能の追加

--- Assets/Scripts/ManagerScript/Pause.cs
+++ Assets/Scripts/ManagerScript/Pause.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Pause : MonoBehaviour
 {
+    [Header("キャンバス")]
+    [SerializeField] GameObject[] canvas;
+
+    [Header("ポーズメニューのカーソル初期位置")]
+    [SerializeField] GameObject focusPauseMenu;
+
+    GameObject currentFocus;    //現在
+    GameObject previousFocus;   //前フレーム
 
     bool pause;
 
     void Start()
     {
-        
+        CanvasInit();
+        canvas[0].SetActive(true);
     }
 
     void Update()
@@ -22,6 +32,7 @@ public class Pause : MonoBehaviour
         {
             PauseGame();
         }
+        FocusCheck();
     }
 
     public void PauseGame()
@@ -32,6 +43,9 @@ public class Pause : MonoBehaviour
             Time.timeScale = 0;
             pause = true;
             Debug.Log("一時停止");
+            canvas[1].SetActive(true);
+            canvas[0].SetActive(false);
+            EventSystem.current.SetSelectedGameObject(focusPauseMenu);
         }
     }
     public void StartGame()
@@ -42,7 +56,38 @@ public class Pause : MonoBehaviour
             Time.timeScale = 1;
             pause = false;
             Debug.Log("再開");
+            canvas[0].SetActive(true);
+            canvas[1].SetActive(false);
         }
     }
-    
+
+    void CanvasInit()
+    {
+        for (int i = 0; i < canvas.Length; i++)
+        {
+            canvas[i].SetActive(false);
+        }
+    }
+
+    void FocusCheck()
+    {
+        //現在のフォーカスを格納
+        currentFocus = EventSystem.current.currentSelectedGameObject;
+
+        //もし前回までのフォーカスと同じなら即終了
+        if (currentFocus == previousFocus) return;
+
+        //もしフォーカスが外れていたら
+        //前フレームのフォーカスに戻す
+        if (currentFocus == null)
+        {
+            EventSystem.current.SetSelectedGameObject(previousFocus);
+            return;
+        }
+
+        //残された条件から、フォーカスが存在するのは確定
+        //前フレームのフォーカスを更新
+        previousFocus = EventSystem.current.currentSelectedGameObject;
+    }
+
 }
